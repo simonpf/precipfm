dd9ba105ca4fe9b2009fd57b011bd00a370d790a
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2025-03-24 14:37:13 -0600

    Add function to plot tiles.

--- src/precipfm/plotting.py
+++ src/precipfm/plotting.py
@@ -126,3 +126,39 @@ def plot_tensors_as_animation(
     ani = animation.FuncAnimation(fig, update, frames=len(images), interval=interval, blit=False)
     plt.close(fig)
     return HTML(ani.to_jshtml())
+
+
+def plot_tiles(tnsr: torch.Tensor, global_y, global_x, local_y, local_x, channel: Optional[int] = None):
+    """
+    Plot tiled tensor.
+    """
+    tnsr = tnsr.detach().cpu()
+    f, ax = plt.subplots(1, 1, figsize=(10, 5))
+    gap = 3
+
+    n_tiles_y = tnsr.shape[global_y]
+    n_tiles_x = tnsr.shape[global_x]
+    height = tnsr.shape[local_y]
+    width = tnsr.shape[local_x]
+
+    all_tiles = tnsr.squeeze()
+    if channel is not None:
+        all_tiles = all_tiles[..., channel, :, :]
+    norm = Normalize(np.nanmin(all_tiles), np.nanmax(all_tiles))
+
+    for tile_ind_y in range(n_tiles_y):
+        for tile_ind_x in range(n_tiles_x):
+            tile = tnsr.select(global_y, tile_ind_y).select(global_x - 1, tile_ind_x).squeeze()
+            if channel is not None:
+                tile = tile[channel]
+
+            start = (width + gap) * tile_ind_x
+            end = start + width
+            x = np.arange(start, end)
+            start = (height + gap) * tile_ind_y
+            end = start + height
+            y = np.arange(start, end)
+
+            m = ax.pcolormesh(x, y, tile, norm=norm)
+
+    plt.colorbar(m)
