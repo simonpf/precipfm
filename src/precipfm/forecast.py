34e9e44370daf31f21b8b311b086e333561988a7
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2025-04-17 21:42:24 -0600

    Add ability to calculate diagnostics in forecaster.

--- src/precipfm/forecast.py
+++ src/precipfm/forecast.py
@@ -4,9 +4,15 @@ precipfm.forecast
 
 Provides functionality to perform forecasts with the PrithviWxC foundation model.
 """
+from typing import Dict, Optional
+from functools import partial
+
 import numpy as np
+from pytorch_retrieve.architectures import RetrievalModel
+from pytorch_retrieve.inference import run_inference
 import torch
 from torch import nn
+from tqdm import tqdm
 import xarray as xr
 
 
@@ -39,7 +45,8 @@ class Forecaster:
             t_0: np.datetime64,
             n_steps: int,
             device: str = "cuda",
-            dtype: str = "float32"
+            dtype: str = "float32",
+            diagnostics: Optional[Dict[str, RetrievalModel]] = None
     ):
         """
         Run forecast for given initialization time 't_0'.
@@ -71,10 +78,10 @@ class Forecaster:
 
         mr_tmp = model.mask_ratio_inputs
 
-        print(mr_tmp)
+        diagnosed = {}
 
         with torch.no_grad():
-            for step in range(n_steps):
+            for step in tqdm(range(n_steps)):
 
                 x["static"] = static_input[[step]].to(device=device, dtype=dtype)
                 x["climate"] = climate_input[[step]].to(device=device, dtype=dtype)
@@ -84,6 +91,38 @@ class Forecaster:
 
                 pred = model(x)
 
+
+                if 0 < len(diagnostics):
+                    step_time = t_0 + np.timedelta64(lead_time, "h") * (1 + step)
+                    pad = partial(nn.functional.pad, pad=((0, 0, 0, -1)))
+                    static = pad(torch.tensor(self.input_loader.load_static_data(step_time)))[None]
+                    static = static.to(device=device, dtype=dtype)
+                    static_mu = model.static_input_scalers_mu
+                    static_sigma = model.static_input_scalers_sigma
+                    static = torch.cat(
+                        (static[:, :2], (static[:, 2:] - static_mu[:, 3:]) / static_sigma[:, 3:]),
+                        1
+                    )
+
+                    dynamic_mu = model.input_scalers_mu[0]
+                    dynamic_sigma = model.input_scalers_sigma[0]
+                    dynamic = (pred - dynamic_mu) / dynamic_sigma
+
+                    inpt = {
+                        "static": static,
+                        "dynamic": dynamic
+                    }
+
+                    for diag_name, diag_model in diagnostics.items():
+                        diagnosed.setdefault(diag_name, []).append(
+                            run_inference(
+                                diag_model,
+                                inpt,
+                                diag_model.inference_config,
+                                device=device,
+                                dtype=dtype
+                            )[0]
+                        )
 
                 results.append(pred.cpu().float().numpy()[0])
                 x_new = torch.stack([x["x"][:, -1], pred], 1)
@@ -115,6 +154,14 @@ class Forecaster:
             slc = slice(20 + var_ind * 14, 20 + (var_ind + 1) * 14)
             dataset[var + "_CLI"] = (("time", "levels", "latitude", "longitude"), clim[:, slc])
 
+        all_diagnosed = []
+        for name, results in diagnosed.items():
+            results = xr.concat(results, "time")
+            results["time"] = (("time"), times)
+            all_diagnosed.append(results)
+
+        dataset = xr.merge([dataset,] + all_diagnosed)
+
         try:
             output = self.input_loader.get_forecast_output(t_0, n_steps)
             for var_ind, var in enumerate(SURFACE_VARS):
@@ -123,8 +170,7 @@ class Forecaster:
             for var_ind, var in enumerate(VERTICAL_VARS):
                 slc = slice(20 + var_ind * 14, 20 + (var_ind + 1) * 14)
                 dataset[var + "_REF"] = (("time", "levels", "latitude", "longitude"), output[:, slc])
-        except AttributeError as exc:
-            raise exc
+        except Exception as exc:
 
             pass
 
